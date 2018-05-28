b711568b9ea5f2c8d8fbe597da9f2d4b5a965ce2
Pedja <pedja.zikic@gmail.com>
2018-05-28 12:46:54 +0200

    users page working without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,7 +18,10 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -25,7 +31,11 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
+       $roles = Role::lists('name','id')->all();
+
+
+
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -34,9 +44,17 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        $input = $request->all();
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
+        return redirect('/admin/users');
+
+
     }
 
     /**
@@ -58,6 +76,8 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
+
+
         return view('admin.users.edit');
     }
 
